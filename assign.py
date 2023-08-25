92d15c344d723aae7b03c0b6f4d0026de49df3d8
utkarsh-G-23 <118457463+utkarsh-G-23@users.noreply.github.com>
2023-08-25 10:03:05 +0530

    test 2

---
+++ assign.py
@@ -0,0 +1,7 @@
+P_ID = ["P1","P23","P93","P42","P9","P87"]
+Process = ["VSCode","Eclipse","Chrome","JDK","CMD","NotePad"]
+Start_Time = [100,234,189,9,7,23]
+Priority = ["MID","MID","High","High","High","Low"]
+
+def print_menu():
+    print("Select which parameter you want to SORT the data on? ()")
